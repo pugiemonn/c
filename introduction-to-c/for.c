12bdfea697c119f70c2ef1d29439b180d79c87fc
Pugiemonn <pugiemonn@gmail.com>
2012-09-11 19:46:42 +0900

    5000回のループを判定して抜けるように変更

--- introduction-to-c/for.c
+++ introduction-to-c/for.c
@@ -3,6 +3,20 @@
 int main()
 {
     int count;
-    for (count = 1; count <= 10; count++)printf("%d回目のループです\n", count);
+    int max;
+
+    printf("ループ回数を指定してください\n");
+    scanf("%d", &max);
+
+    for (count = 1; count <= max; count++)
+    {
+        printf("%d回目のループです\n", count);
+        if(count == 5000)
+        {
+            printf("5000回を越えたので強制終了します\n");
+            count = max;
+        }
+    }
+    printf("ループを抜けました\n");
     return 0;
 }
