56b5a6e98c1a0e458ffb8f9ca4dcb6670e836e9b
Pugiemonn <pugiemonn@gmail.com>
2012-09-11 16:56:36 +0900

    変数に数字を代入

--- introduction-to-c/variable_number.c
+++ introduction-to-c/variable_number.c
@@ -2,5 +2,9 @@
 
 main()
 {
-  printf("数字を表示するホ%d\n", 44);
+    char x = 'X';
+    float ran = 42.195;
+    printf("数字を表示するホ%d\n", 44);
+    printf("%c感じてみろー\n", x);
+    printf("フルマラソンは %fKm\n", ran);
 }
